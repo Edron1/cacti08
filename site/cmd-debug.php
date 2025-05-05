8e027871ac223d7f8ebd772efa44777f849b9856
Edron <edronmcposht@gmail.com>
2025-05-05 15:52:07 +0300

    first commit

---
+++ site/cmd-debug.php
@@ -0,0 +1,11 @@
+<?php
+
+$_SESSION["sess_config_array"]["log_verbosity"] = 10;
+$config["config_options_array"]["log_verbosity"] = 10;
+
+var_dump($_SERVER["argv"]);
+
+include 'cmd1.php';
+
+
+?>
---
+++ site/include/csrf/csrf-secret.php
@@ -0,0 +1,1 @@
+<?php $secret = "0721d70f2bec5e9289fcb0f93ab6d0a61f2917c5";
